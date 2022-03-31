aae660541cb50a2668e5e305b290ed3783fa9ae7
sfiolic <54515045+sfiolic@users.noreply.github.com>
2022-03-31 13:45:51 -0500

    Write test for base case if input is empty and write implementation in allPalis

--- Task1/task1.py
+++ Task1/task1.py
@@ -9,15 +9,22 @@
 #               alphabetic letters. I will treat mix cases the same by
 #               checking on the lower case letters only.
 def isPali(input_string):
+    #check if string type
     if type(input_string) == str:
+        #check if input is same reversed
         return input_string.lower() == input_string.lower()[::-1]
     return False
 
 #Assumptions: The string check happens in the inner functions. It would
 #               it would make more sense to have it in allPalis. I am assuming
-#               that all the inputs into allPalis are valid strings. 
+#               that all the inputs into allPalis are valid strings.
 def allPalis(input_string):
-    return 0
+    res = []
+    # if input is empty then append empty list
+    if not input_string:
+        res.append([])
+
+    return res
 
 #################################################################
 # TEST
@@ -45,6 +52,10 @@ def test_allpali_5():
     expected = [ [ 5, 8, 'ana' ] ]
     return allPalis("anavolimilovana") == expected
 
+def test_allpali_6():
+    expected = [ [] ]
+    return allPalis("") == expected
+
 
 #################################################################
 # MAIN
@@ -60,6 +71,8 @@ def main():
             "Pass" if test_allpali_4() else "Fail")
     print("test 5, list equals, should be True:    ",
             "Pass" if test_allpali_5() else "Fail")
+    print("test 6, list equals, should be True:    ",
+            "Pass" if test_allpali_6() else "Fail")
 
 if __name__ == '__main__':
     main()
