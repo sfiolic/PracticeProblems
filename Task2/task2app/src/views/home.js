ea01730cee9b811b0be054a8cbc02dd324466bfd
sfiolic <54515045+sfiolic@users.noreply.github.com>
2022-04-05 11:36:34 -0500

    Add home and profile page

---
+++ Task2/task2app/src/views/home.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+import { Hero, HomeContent } from "../components";
+
+const Home = () => (
+  <Fragment>
+    <Hero />
+    <hr />
+    
+  </Fragment>
+);
+
+export default Home;
