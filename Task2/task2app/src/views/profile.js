c8ea134c7a6ba888cc5991b7aef2949fb4d393c4
sfiolic <54515045+sfiolic@users.noreply.github.com>
2022-04-05 14:38:45 -0500

    Add conditional for the profile page when user is logged in and logged out

--- Task2/task2app/src/views/profile.js
+++ Task2/task2app/src/views/profile.js
@@ -1,8 +1,39 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
+import { ContactFormComponent} from "../components";
 
 const Profile = () => {
+  const { user } = useAuth0();
 
   return (
+    <div>
+    {user && (
+      <div>
+      <div className="row align-items-center profile-header">
+        <div className="col-md-2 mb-3">
+          <img
+            src={user.picture}
+            alt="Profile"
+            className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
+          />
+        </div>
+        <div className="col-md text-center text-md-left">
+          <h2>{user.name}</h2>
+          <p className="lead text-muted">{user.email}</p>
+        </div>
+      </div>
+      <div className="row">
+        <pre className="col-12 text-light bg-dark p-4">
+          {JSON.stringify(user, null, 2)}
+        </pre>
+      </div>
+
+
+    <ContactFormComponent/>
+
+    </div>
+  )}
+    {!user && (
       <div>
       <h1>Profile</h1>
       <p>
@@ -12,6 +43,8 @@ const Profile = () => {
         <strong>This route should be protected</strong>.
       </p>
     </div>
+  )}
+  </div>
   );
 };
 
