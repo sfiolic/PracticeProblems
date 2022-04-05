30a723669eba620bece7ad0aa7fca37fe996c67e
sfiolic <54515045+sfiolic@users.noreply.github.com>
2022-04-05 14:39:17 -0500

    Index a new page that is used for forms

--- Task2/task2app/src/components/index.js
+++ Task2/task2app/src/components/index.js
@@ -1,7 +1,8 @@
 
 import Footer from "./footer";
-import Hero from "./hero";
+import ContactFormComponent from "./hero";
 import Loading from "./loading";
 import NavBar from "./nav-bar";
 
-export { Footer, Hero, Loading, NavBar };
+
+export { Footer, ContactFormComponent, Loading, NavBar};
