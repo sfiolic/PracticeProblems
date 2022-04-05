ac8e1f90df01e12b0baec697f8a12dc7b7e65bbb
sfiolic <54515045+sfiolic@users.noreply.github.com>
2022-04-05 09:07:07 -0500

    Do testing with sample auth0 app, now update the package.json to have versions inline with sample app

--- Task2/task2app/src/App.js
+++ Task2/task2app/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Auth0ProviderWithHistory from "./auth0Provider"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Auth0ProviderWithHistory>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Auth0ProviderWithHistory>
   );
 }
 
