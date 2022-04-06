a332707bea09bfb3f6d3e628b8693cab07760964
sfiolic <54515045+sfiolic@users.noreply.github.com>
2022-04-05 22:41:05 -0500

    Utilize the resolvers and form connection with the server

--- Task3/task3app/src/index.js
+++ Task3/task3app/src/index.js
@@ -1,5 +1,9 @@
 const { ApolloServer } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 const typeDefs = `
   type Query {
@@ -24,8 +28,35 @@ let links = [{
   description: 'Fullstack tutorial for GraphQL'
 }]
 
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Link
+}
 // 3
+const prisma = new PrismaClient()
+const fs = require('fs');
+const path = require('path');
+const { getUserId } = require('./utils');
 
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
+  resolvers,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      userId:
+        req && req.headers.authorization
+          ? getUserId(req)
+          : null
+    };
+  }
+});
 
 server
   .listen()
