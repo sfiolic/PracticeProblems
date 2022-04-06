8a58a529e96d087c9dc0161a3491afd55087cacb
sfiolic <54515045+sfiolic@users.noreply.github.com>
2022-04-05 22:39:36 -0500

    Commit migrations for the prisma

---
+++ Task3/task3app/prisma/migrations/20220405234211_detail_user_model/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "firstname" TEXT NOT NULL DEFAULT '',
+    "lastname" TEXT NOT NULL DEFAULT '',
+    "address" TEXT NOT NULL DEFAULT '',
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+INSERT INTO "new_User" ("email", "id", "password") SELECT "email", "id", "password" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE TABLE "new_Link" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "description" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "postedById" INTEGER
+);
+INSERT INTO "new_Link" ("createdAt", "description", "id", "postedById", "url") SELECT "createdAt", "description", "id", "postedById", "url" FROM "Link";
+DROP TABLE "Link";
+ALTER TABLE "new_Link" RENAME TO "Link";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
